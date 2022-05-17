d47305b8087c4c6dfff1007bc8357a36b5626211
Avichay Vaknin <79922086+AV2606@users.noreply.github.com>
2022-05-18 00:52:30 +0300

    Add project files.

---
+++ FlowersHW/Controllers/FlowersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlowersHW.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class FlowersController:ControllerBase
+    {
+
+        public string Rose()
+        {
+            return JsonSerializer.Serialize(new { name = "Rose" });
+        }
+        public string Sunflower()
+        {
+            return JsonSerializer.Serialize(new { name = "SunFlower" });
+        }
+        public string Hyacinth()
+        {
+            return JsonSerializer.Serialize(new { name = "hyacinth" });
+        }
+        public string Narcissus()
+        {
+            return JsonSerializer.Serialize(new { name = "narcissus" });
+        }
+    }
+}
